5c59fb7c46d05f01754d25709024f62130bd66f9
FelipeMDB <>
2023-10-09 20:19:05 -0300

    Update algoritmos_ordenacao.h

--- src/algoritmos/algoritmos_ordenacao.h
+++ src/algoritmos/algoritmos_ordenacao.h
@@ -2,15 +2,22 @@
 #define algoritmos_ordenacao
 
 // Função para ordenar um array usando o algoritmo Insertion Sort
-void insertionSort(int arr[], int n);
+void insertionSort(int vetor[], int n);
 
 // Função para ordenar um array usando o algoritmo Selection Sort
-void selectionSort(int arr[], int n);
+void selectionSort(int vetor[], int n);
 
 // Função para ordenar um array usando o algoritmo Merge Sort
-void mergeSort(int arr[], int l, int r);
+void mergeSort(int vetor[], int l, int r);
 
 // Função para ordenar um array usando o algoritmo Quick Sort
-void quickSort(int arr[], int low, int high);
+void quickSort(int vetor[], int low, int high);
 
 #endif /* algoritmos_ordenacao */
+
+#ifndef gerador
+#define gerador
+
+int* gerarVetorAleatorio(int tamanho);
+
+#endif
