8a658afcfd4e989573dc2b454fea79588d5a9d49
FelipeMDB <>
2023-10-09 19:32:26 -0300

    Update insertion_sort.c
    adicionando contador de tempo

--- src/algoritmos/insertion_sort.c
+++ src/algoritmos/insertion_sort.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
+#include <time.h>
 
-void insertionSort(int vetor[], int n) {
-    int i, chave, j, trocas = 0; // Inicialize o contador de trocas como 0
+void insertionSort(int vetor[], int n, int *trocas) {
+    clock_t inicio = clock();
+    int i, chave, j;
     for (i = 1; i < n; i++) {
         chave = vetor[i];
         j = i - 1;
 
-        // Move os elementos da parte ordenada da matriz
-        // que são maiores que a chave, para uma posição à frente de sua posição atual.
         while (j >= 0 && vetor[j] > chave) {
             vetor[j + 1] = vetor[j];
             j = j - 1;
-            trocas++; // Incrementa o contador de trocas
+            (*trocas)++;
         }
         vetor[j + 1] = chave;
     }
-
-    printf("Número de trocas: %d\n", trocas); // Imprime o número total de trocas
+    clock_t fim = clock();
+    double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do Insertion Sort: %.6f segundos\n", tempo);
 }
 
 int main() {
     int vetor[] = {12, 11, 13, 5, 6};
     int n = sizeof(vetor) / sizeof(vetor[0]);
+    int trocas = 0;
 
-    insertionSort(vetor, n);
-
-    printf("Vetor ordenado: \n");
+    printf("Vetor desordenado: \n");
     for (int i = 0; i < n; i++) {
         printf("%d ", vetor[i]);
     }
-    printf("\n");
+
+    insertionSort(vetor, n, &trocas);
+
+    printf("\nVetor ordenado: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\nNúmero de trocas: %d\n", trocas);
     return 0;
 }
