fc5af23c3ea3801fa843e28c1af3ffa9eec8bdda
FelipeMDB <>
2023-10-09 20:10:56 -0300

    mergesort
    mergesort 100% funcional com trocas e timing

--- src/algoritmos/merge_sort.c
+++ src/algoritmos/merge_sort.c
@@ -1,34 +1,54 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-// Função para mesclar dois subarrays ordenados em um único array ordenado
-void merge(int arr[], int l, int m, int r, int *trocas) {
+// Função para gerar um vetor com valores aleatórios
+int* gerarVetorAleatorio(int tamanho) {
+    int* vetor = (int*)malloc(tamanho * sizeof(int));
+    if (vetor == NULL) {
+        // Tratar erro de alocação de memória
+        return NULL;
+    }
+
+    // Inicializar a semente para números aleatórios
+    srand(time(NULL));
+
+    for (int i = 0; i < tamanho; i++) {
+        vetor[i] = rand(); // Preencher o vetor com números aleatórios
+    }
+
+    return vetor;
+}
+
+// Função para mesclar dois subvetores ordenados em um único vetor ordenado
+void merge(int *vetor, int l, int m, int r, int *trocas) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Cria arrays temporários
-    int L[n1], R[n2];
+    // Cria vetores temporários
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
 
-    // Copia os dados para os arrays temporários L[] e R[]
+    // Copia os dados para os vetores temporários L[] e R[]
     for (i = 0; i < n1; i++) {
-        L[i] = arr[l + i];
+        L[i] = vetor[l + i];
     }
     for (j = 0; j < n2; j++) {
-        R[j] = arr[m + 1 + j];
+        R[j] = vetor[m + 1 + j];
     }
 
-    // Mescla os arrays temporários de volta em arr[l..r]
-    i = 0; // Índice inicial do primeiro subarray
-    j = 0; // Índice inicial do segundo subarray
-    k = l; // Índice inicial do subarray mesclado
+    // Mescla os vetores temporários de volta em vetor[l..r]
+    i = 0; // Índice inicial do primeiro subvetores
+    j = 0; // Índice inicial do segundo subvetores
+    k = l; // Índice inicial do subvetores mesclado
 
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
-            arr[k] = L[i];
+            vetor[k] = L[i];
             i++;
         } else {
-            arr[k] = R[j];
+            vetor[k] = R[j];
             j++;
             (*trocas)++; // Incrementa o contador de trocas
         }
@@ -37,57 +57,70 @@ void merge(int arr[], int l, int m, int r, int *trocas) {
 
     // Copia os elementos restantes de L[], se houver algum
     while (i < n1) {
-        arr[k] = L[i];
+        vetor[k] = L[i];
         i++;
         k++;
     }
 
     // Copia os elementos restantes de R[], se houver algum
     while (j < n2) {
-        arr[k] = R[j];
+        vetor[k] = R[j];
         j++;
         k++;
     }
+
+    // Libere a memória dos vetores temporários
+    free(L);
+    free(R);
 }
 
 // Função principal para executar o Merge Sort
-void mergeSort(int arr[], int l, int r, int *trocas) {
-    clock_t inicio = clock();
+void mergeSort(int *vetor, int l, int r, int *trocas) {
     if (l < r) {
-        // Encontra o ponto médio do array
+        // Encontra o ponto médio do vetor
         int m = l + (r - l) / 2;
 
         // Classifica a metade esquerda
-        mergeSort(arr, l, m, trocas);
+        mergeSort(vetor, l, m, trocas);
 
         // Classifica a metade direita
-        mergeSort(arr, m + 1, r, trocas);
+        mergeSort(vetor, m + 1, r, trocas);
 
         // Mescla as duas metades classificadas
-        merge(arr, l, m, r, trocas);
+        merge(vetor, l, m, r, trocas);
     }
-    clock_t fim = clock();
-    double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("Tempo de execução do Merge Sort: %.6f segundos\n", tempo);
 }
 
 int main() {
-    int arr[] = {38, 27, 43, 3, 9, 82, 10};
-    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int qtdItems = 10000000;
+    int *vetor = gerarVetorAleatorio(qtdItems);
+    int n = qtdItems; // Tamanho do vetor
     int trocas = 0; // Inicialize o contador de trocas como 0
 
-    printf("Array desordenado: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    // printf("Vetor desordenado:\n");
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", vetor[i]);
+    // }
 
-    mergeSort(arr, 0, n - 1, &trocas);
+    
+    clock_t inicio = clock();
 
-    printf("\nArray ordenado: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    mergeSort(vetor, 0, n - 1, &trocas);
+
+    clock_t fim = clock();
+    double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+    // printf("\nVetor ordenado:\n");
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", vetor[i]);
+    // }
 
     printf("\nNúmero de trocas: %d\n", trocas); // Imprime o número total de trocas
+    printf("Tempo de execução do Merge Sort: %.6f segundos\n", tempo);
+
+    // Libere a memória alocada para o vetor
+    free(vetor);
+
     return 0;
 }
